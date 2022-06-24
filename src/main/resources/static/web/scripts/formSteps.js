33082a3a02cf8bab58440954dfa6ee5142895b80
davey-x1 <david.pereira.ar1@gmail.com>
2022-06-24 10:25:08 -0500

    formSteps
    formSteps

--- src/main/resources/static/web/scripts/formSteps.js
+++ src/main/resources/static/web/scripts/formSteps.js
@@ -46,3 +46,59 @@ function nextIcon(){
       n++;
     }
 }
+
+function switchTab(number){
+  let tabs = Array.from($('#main-form').children('.tab-steps'));
+  let icons = Array.from($('.first ul li').children('a').children('span'));
+  if(number > tabs.length){
+    return false;
+    console.log("Invalid data");
+  }
+  var i = 0;
+  while(i < tabs.length){
+    console.log(i);
+    if(i == number){
+      if($(tabs[i]).hasClass('hidden') == true){
+        $(tabs[i]).toggleClass('hidden');
+
+      }
+      i++;
+    } else {
+      if($(tabs[i]).hasClass('hidden') == false){
+        $(tabs[i]).toggleClass('hidden');
+
+      }
+      i++;
+    }
+  }
+
+  var j = 0;
+  while(j < icons.length){
+    console.log(j);
+    if(j > number){
+      console.log("mayor");
+      if($(icons[j]).hasClass('active') == true){
+        $(icons[j]).toggleClass('active');
+      }
+      j++;
+    } else {
+      if($(icons[j]).hasClass('active') == false){
+        $(icons[j]).toggleClass('active');
+      }
+      j++;
+    }
+  }
+}
+
+
+  /*for(let i = 0; i < tabs.length; i++){
+    if(i != n){
+      if($(tabs[i]).hasClass('hidden') == false){
+        $(tabs[i]).toggleClass('hidden');
+      }
+    } else {
+      if($(tabs[i]).hasClass('hidden') == true){
+        $(tabs[i]).toggleClass('hidden');
+      }
+    }
+  }*/
