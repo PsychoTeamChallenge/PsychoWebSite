afd063e2803786ea93041c911025190783806451
davey-x1 <david.pereira.ar1@gmail.com>
2022-06-29 13:53:07 -0500

    Added favourites and posts logic
    Added logic for favourites and posts, now they can be removed from their respective lists

--- src/main/resources/static/web/scripts/sidebar.js
+++ src/main/resources/static/web/scripts/sidebar.js
@@ -39,11 +39,54 @@ Vue.createApp({
       removePost(id){
         axios.patch("/api/post/delete/" + id)
         .then((response) => {
-          console.log(response);
+          Swal.fire(
+            'Removed!',
+            'This post has been deleted.',
+            'success'
+          );
+          this.refreshSession();
         })
         .catch((error) => {
-          console.log(error);
+          Swal.fire(
+            'Error!',
+            'Error: ' + error,
+            'error'
+          );
+          this.refreshSession();
         })
+      },
+      removeFavourite(id){
+        axios.patch("/api/clients/current/favourites", "idProduct=" + id)
+        .then((response) => {
+          Swal.fire(
+            'Removed!',
+            'This product has been removed from your favourites list',
+            'success'
+          );
+          this.refreshSession();
+        })
+        .catch((error) => {
+          Swal.fire(
+            'Error!',
+            'Error: ' + error,
+            'error'
+          );
+          this.refreshSession();
+        })
+      },
+      refreshSession(){
+        axios.get("/api/post").then(response => {
+            this.posts = response.data;
+            this.postsFiltrados = this.posts;
+        })
+            .catch(error => console.log(error))
+
+        axios.get("/api/clients/current")
+            .then(response => {
+                this.client = response.data;
+                this.isClient = true;
+            })
+            .catch(error => console.log(error))
       }
     },
     computed: {
